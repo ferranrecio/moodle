50dd407e206330b11d8608a6c5d988069fc8798d
Sara Arjona <sara@moodle.com>
2021-03-25 08:59:15 +0100

    MDL-71165 course: core_course_update_course external method

--- lib/db/services.php
+++ lib/db/services.php
@@ -516,6 +516,14 @@ $functions = array(
         'ajax'          => 'true',
         'services'    => [MOODLE_OFFICIAL_MOBILE_SERVICE],
     ],
+    'core_course_update_course' => [
+        'classname'     => 'core_course\external\update_course',
+        'methodname'    => 'execute',
+        'description'   => 'Update course contents.',
+        'type'          => 'write',
+        'ajax'          => true,
+        'capabilities'  => 'moodle/course:sectionvisibility, moodle/course:activityvisibility',
+    ],
     'core_course_edit_module' => array(
         'classname'   => 'core_course_external',
         'methodname'  => 'edit_module',
