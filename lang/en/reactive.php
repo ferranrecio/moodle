9a3f2254288eaea8bc39103c05f13eff976140be
Ferran Recio <ferran@moodle.com>
2021-06-28 13:32:00 +0200

    MDL-71995 core: reactive debug tools

---
+++ lang/en/reactive.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for core reactive development tools.
+ *
+ * @package   core
+ * @copyright 2021 Ferran Recio <ferran@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['instances'] = 'Reactive instances:';
+$string['noinstances'] = 'this page has no reactive instances';
+$string['pin'] = 'Pin';
+$string['unpin'] = 'Unpin';
+$string['highlightoff'] = 'Highlight OFF';
+$string['highlighton'] = 'Highlight ON';
+$string['readmodeon'] = 'Read mode ON';
+$string['readmodeoff'] = 'Read mode OFF';
+$string['resetpanel'] = 'Reset panel';
+$string['statedata'] = 'State data';
+$string['saveingwarning'] = 'Edit the state can cause inexpected results';
+$string['close'] = 'Close';
+
