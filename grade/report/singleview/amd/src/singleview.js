1725d0370ab3cf296a6e8ba2f05a3fedaf172000
ferran <ferranrecio@gmail.com>
2025-01-28 15:14:43 +0100

    fixup! MDL-84071 gradereport_singleview: Rewrite singleview to native JS

--- grade/report/singleview/amd/src/singleview.js
+++ grade/report/singleview/amd/src/singleview.js
@@ -20,175 +20,201 @@
  * @copyright The Open University
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-const GradeReportSingleView = {
-    /**
-     * Indicates if the module has been initialized.
-     * @type {boolean}
-     */
-    initialised: false,
 
-    /**
-     * Initializes the module, setting up event listeners for table cell navigation and override toggles.
-     */
-    init() {
-        if (this.initialised) {
-            return;
-        }
-        this.initialised = true;
-
-        /**
-         * Helper function to get the column index of a cell.
-         *
-         * @param {HTMLElement} cell The table cell element.
-         * @returns {number} The index of the cell within its row.
-         */
-        const getColumnIndex = (cell) => {
-            const rowNode = cell.closest('tr');
-            if (!rowNode || !cell) {
-                return -1;
-            }
-            const cells = Array.from(rowNode.querySelectorAll('td, th'));
-            return cells.indexOf(cell);
-        };
-
-        /**
-         * Helper function to get the next cell in the table.
-         *
-         * @param {HTMLElement} cell The current table cell element.
-         * @returns {HTMLElement|null} The next navigable cell or null if none found.
-         */
-        const getNextCell = (cell) => {
-            const n = cell || document.activeElement;
-            const next = n.nextElementSibling?.matches('td.cell, th.cell') ? n.nextElementSibling : null;
-            if (!next) {
-                return null;
-            }
-            // Continue until we find a navigable cell
-            if (!next.querySelector('input:not([type="hidden"]):not([disabled="DISABLED"]), select, a')) {
-                return getNextCell(next);
-            }
-            return next;
-        };
-
-        /**
-         * Helper function to get the previous cell in the table.
-         *
-         * @param {HTMLElement} cell The current table cell element.
-         * @returns {*|Element|null}
-         */
-        const getPrevCell = (cell) => {
-            const n = cell || document.activeElement;
-            const prev = n.previousElementSibling?.matches('td.cell, th.cell') ? n.previousElementSibling : null;
-            if (!prev) {
-                return null;
-            }
-            // Continue until we find a navigable cell
-            if (!prev.querySelector('input:not([type="hidden"]):not([disabled="DISABLED"]), select, a')) {
-                return getPrevCell(prev);
-            }
-            return prev;
-        };
-
-        /**
-         * Helper function to get the cell above the current cell in the table.
-         *
-         * @param {HTMLElement} cell The current table cell element.
-         * @returns {HTMLElement|null} The cell above or null if none found.
-         */
-        const getAboveCell = (cell) => {
-            const n = cell || document.activeElement;
-            const tr = n.closest('tr').previousElementSibling;
-            const columnIndex = getColumnIndex(n);
-            if (!tr) {
-                return null;
-            }
-            const next = tr.querySelectorAll('td, th')[columnIndex];
-            // Continue until we find a navigable cell
-            if (!next?.querySelector('input:not([type="hidden"]):not([disabled="DISABLED"]), select, a')) {
-                return getAboveCell(next);
-            }
-            return next;
-        };
-
-        /**
-         * Helper function to get the cell below the current cell in the table.
-         *
-         * @param {HTMLElement} cell The current table cell element.
-         * @returns {HTMLElement|null} The cell below or null if none found.
-         */
-        const getBelowCell = (cell) => {
-            const n = cell || document.activeElement;
-            const tr = n.closest('tr').nextElementSibling;
-            const columnIndex = getColumnIndex(n);
-            if (!tr) {
-                return null;
-            }
-            const next = tr.querySelectorAll('td, th')[columnIndex];
-            // Continue until we find a navigable cell
-            if (!next?.querySelector('input:not([type="hidden"]):not([disabled="DISABLED"]), select, a')) {
-                return getBelowCell(next);
-            }
-            return next;
-        };
-
-        // Add ctrl+arrow controls for navigation.
-        document.body.addEventListener('keydown', (e) => {
-            if (!e.ctrlKey) {
-                return;
-            }
-
-            const activeElement = document.activeElement;
-            if (!activeElement.matches('table input, table select, table a')) {
-                return;
-            }
-
-            let next = null;
-            switch (e.keyCode) {
-                case 37: // Left arrow
-                    next = getPrevCell(activeElement.closest('td, th'));
-                    break;
-                case 38: // Up arrow
-                    next = getAboveCell(activeElement.closest('td, th'));
-                    break;
-                case 39: // Right arrow
-                    next = getNextCell(activeElement.closest('td, th'));
-                    break;
-                case 40: // Down arrow
-                    next = getBelowCell(activeElement.closest('td, th'));
-                    break;
-            }
-
-            if (next) {
-                e.preventDefault();
-                e.stopPropagation();
-                next.querySelector('input:not([type="hidden"]):not([disabled="DISABLED"]), select, a')?.focus();
-            } else {
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        });
-
-        // Handle override toggles
-        document.querySelectorAll('input[name^=override_]').forEach(input => {
-            input.addEventListener('change', () => {
-                const checked = input.checked;
-                const [, itemid, userid] = input.getAttribute('name').split('_');
-                const interest = `_${itemid}_${userid}`;
-
-                // Handle text inputs
-                document.querySelectorAll(`input[name$='${interest}'][data-uielement='text']`)
-                    .forEach(text => {
-                        text.disabled = !checked;
-                    });
-
-                // Handle select elements
-                document.querySelectorAll(`select[name$='${interest}']`)
-                    .forEach(select => {
-                        select.disabled = !checked;
-                    });
-            });
-        });
-    }
+const selectors = {
+    cell: 'td.cell, th.cell',
+    col: 'td, th',
+    keyboardHandled: 'table input, table select, table a',
+    navigableCell: 'input:not([type="hidden"]):not([disabled]), select, a',
+    override: 'input[name^=override_]',
+    row: 'tr',
+    // Dyanmic selectors.
+    input: (interest) => `input[name$='${interest}'][data-uielement='text']`,
+    select: (interest) => `select[name$='${interest}']`,
 };
 
-export default GradeReportSingleView;
+let initialized = false;
+
+/**
+ * Initializes the module, setting up event listeners for table cell navigation and override toggles.
+ */
+export function init() {
+    if (initialized) {
+        return;
+    }
+    initialized = true;
+
+    // Add ctrl+arrow controls for navigation.
+    document.body.addEventListener('keydown', keydownHandler);
+
+    // Handle override toggles.
+    document.querySelectorAll(selectors.override).forEach(input => {
+        input.addEventListener('change', () => {
+            updateOverrideToggle(input);
+        });
+    });
+}
+
+/**
+ * Handles control+arrow table navigation.
+ *
+ * @private
+ * @param {KeyboardEvent} event The keydown event.
+ */
+function keydownHandler(event) {
+    if (!event.ctrlKey) {
+        return;
+    }
+
+    const activeElement = document.activeElement;
+    if (!activeElement.matches(selectors.keyboardHandled)) {
+        return;
+    }
+
+    let next = null;
+    switch (event.key) {
+        case 'ArrowLeft':
+            next = getPrevCell(activeElement.closest(selectors.col));
+            break;
+        case 'ArrowUp':
+            next = getAboveCell(activeElement.closest(selectors.col));
+            break;
+        case 'ArrowRight':
+            next = getNextCell(activeElement.closest(selectors.col));
+            break;
+        case 'ArrowDown':
+            next = getBelowCell(activeElement.closest(selectors.col));
+            break;
+    }
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (next) {
+        next.querySelector(selectors.navigableCell)?.focus();
+    }
+}
+
+/**
+ * Handles changes to override toggles.
+ *
+ * @private
+ * @param {HTMLInputElement} input The override toggle input element.
+ */
+function updateOverrideToggle(input) {
+    const checked = input.checked;
+    const [, itemid, userid] = input.getAttribute('name').split('_');
+    const interest = `_${itemid}_${userid}`;
+
+    // Handle text inputs.
+    document.querySelectorAll(selectors.input(interest)).forEach(
+        text => {
+            text.disabled = !checked;
+        }
+    );
+
+    // Handle select elements.
+    document.querySelectorAll(selectors.select(interest)).forEach(
+        select => {
+            select.disabled = !checked;
+        }
+    );
+}
+
+/**
+ * Helper function to get the next cell in the table.
+ *
+ * @private
+ * @param {HTMLElement} cell
+ * @returns {HTMLElement|null} The next navigable cell or null if none found.
+ */
+function getNextCell(cell) {
+    const checkElement = cell || document.activeElement;
+    const next = checkElement.nextElementSibling?.matches(selectors.cell) ? checkElement.nextElementSibling : null;
+    if (!next) {
+        return null;
+    }
+    // Continue until we find a navigable cell
+    if (!next.querySelector(selectors.navigableCell)) {
+        return getNextCell(next);
+    }
+    return next;
+}
+
+/**
+ * Helper function to get the previous cell in the table.
+ *
+ * @private
+ * @param {HTMLElement} cell
+ */
+function getPrevCell(cell) {
+    const checkElement = cell || document.activeElement;
+    const prev = checkElement.previousElementSibling?.matches(selectors.cell) ? checkElement.previousElementSibling : null;
+    if (!prev) {
+        return null;
+    }
+    // Continue until we find a navigable cell
+    if (!prev.querySelector(selectors.navigableCell)) {
+        return getPrevCell(prev);
+    }
+    return prev;
+}
+
+/**
+ * Helper function to get the cell above the current cell in the table.
+ *
+ * @private
+ * @param {HTMLElement} cell The current table cell element.
+ * @returns {HTMLElement|null} The cell above or null if none found.
+ */
+function getAboveCell(cell) {
+    const checkElement = cell || document.activeElement;
+    const tr = checkElement.closest(selectors.row).previousElementSibling;
+    const columnIndex = getColumnIndex(checkElement);
+    if (!tr) {
+        return null;
+    }
+    const next = tr.querySelectorAll(selectors.col)[columnIndex];
+    // Continue until we find a navigable cell
+    if (!next?.querySelector(selectors.navigableCell)) {
+        return getAboveCell(next);
+    }
+    return next;
+}
+
+/**
+ * Helper function to get the cell below the current cell in the table.
+ *
+ * @private
+ * @param {HTMLElement} cell The current table cell element.
+ * @returns {HTMLElement|null} The cell below or null if none found.
+ */
+function getBelowCell(cell) {
+    const checkElement = cell || document.activeElement;
+    const tr = checkElement.closest('tr').nextElementSibling;
+    const columnIndex = getColumnIndex(checkElement);
+    if (!tr) {
+        return null;
+    }
+    const next = tr.querySelectorAll('td, th')[columnIndex];
+    // Continue until we find a navigable cell
+    if (!next?.querySelector(selectors.navigableCell)) {
+        return getBelowCell(next);
+    }
+    return next;
+}
+
+/**
+ * Helper function to get the column index of a cell.
+ *
+ * @param {HTMLElement} cell
+ * @returns {number} The index of the cell within its row.
+ */
+function getColumnIndex(cell) {
+    const rowNode = cell.closest(selectors.row);
+    if (!rowNode || !cell) {
+        return -1;
+    }
+    const cells = Array.from(rowNode.querySelectorAll(selectors.col));
+    return cells.indexOf(cell);
+}
