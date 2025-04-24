e5b0e5542a303a898b7920d25006721abcee42a5
ferran <ferranrecio@gmail.com>
2025-04-24 11:16:10 +0200

    MDL-84291 course: remove max section limits on user actions

--- course/format/classes/stateactions.php
+++ course/format/classes/stateactions.php
@@ -269,12 +269,6 @@ class stateactions {
 
         // Get course format settings.
         $format = course_get_format($course->id);
-        $lastsectionnumber = $format->get_last_section_number();
-        $maxsections = $format->get_max_sections();
-
-        if ($lastsectionnumber >= $maxsections) {
-            throw new moodle_exception('maxsectionslimit', 'moodle', '', $maxsections);
-        }
 
         $modinfo = get_fast_modinfo($course);
 
