28d711f5683032e1006e3a1555c75dc9331083a9
Laurent David <lmedavid@gmail.com>
2025-07-08 16:22:14 +0200

    MDL-83898 mod_quiz: Add overview page for quiz

---
+++ mod/quiz/classes/courseformat/overview.php
@@ -0,0 +1,191 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_quiz\courseformat;
+
+use core\output\renderer_helper;
+use core\url;
+use cm_info;
+use core_calendar\output\humandate;
+use core_courseformat\local\overview\overviewitem;
+use core\output\action_link;
+use core\output\local\properties\text_align;
+use core\output\local\properties\button;
+use core_courseformat\output\local\overview\overviewdialog;
+use mod_quiz\dates;
+use mod_quiz\quiz_settings;
+
+/**
+ * Wiki overview integration.
+ *
+ * @package    mod_quiz
+ * @copyright  2025 Laurent David <laurent.david@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class overview extends \core_courseformat\activityoverviewbase {
+    /**
+     * @var quiz_settings the quiz settings object.
+     */
+    private quiz_settings $quizsettings;
+
+    /**
+     * Constructor.
+     *
+     * @param cm_info $cm the course module instance.
+     * @param renderer_helper $rendererhelper the renderer helper.
+     * @param \core_string_manager $stringmanager
+     */
+    public function __construct(
+        cm_info $cm,
+        /** @var renderer_helper $rendererhelper the renderer helper */
+        protected readonly renderer_helper $rendererhelper,
+        /** @var \core_string_manager $stringmanager the string manager */
+        protected readonly \core_string_manager $stringmanager,
+    ) {
+        parent::__construct($cm);
+        $this->quizsettings = quiz_settings::create_for_cmid($cm->id);
+    }
+
+    #[\Override]
+    public function get_due_date_overview(): ?overviewitem {
+        global $USER;
+
+        $dates = new dates($this->cm, $USER->id);
+        $duedate = $dates->get_due_date();
+        $name = $this->stringmanager->get_string('duedate', 'quiz');
+
+        if (empty($duedate)) {
+            return new overviewitem(
+                name: $name,
+                value: null,
+                content: '-',
+            );
+        }
+
+        $content = humandate::create_from_timestamp($duedate);
+
+        return new overviewitem(
+            name: $name,
+            value: $duedate,
+            content: $content,
+        );
+    }
+
+    #[\Override]
+    public function get_actions_overview(): ?overviewitem {
+        if (!has_capability('mod/quiz:viewreports', $this->cm->context)) {
+            return null; // If the user cannot manage the quiz, we don't show the quiz type.
+        }
+        $text = $this->stringmanager->get_string('view');
+        $content = new action_link(
+            url: new url(
+                '/mod/quiz/report.php',
+                ['id' => $this->cm->id, 'mode' => 'responses'],
+            ),
+            text: $text,
+            attributes: ['class' => button::SECONDARY_OUTLINE->classes()],
+        );
+        return new overviewitem(
+            name: $this->stringmanager->get_string('actions'),
+            value: '',
+            content: $content,
+            textalign: text_align::CENTER,
+        );
+    }
+
+    #[\Override]
+    public function get_extra_overview_items(): array {
+        if (has_capability('mod/quiz:viewreports', $this->cm->context)) {
+            return [
+                'studentswhoattempted' => $this->get_students_who_attempted_overview(),
+                'totalattempts' => $this->get_total_attempts_overview(),
+            ];
+        }
+        return [];
+    }
+
+    /**
+     * Get the "Students who attempted" item.
+     *
+     * @return overviewitem The overview item.
+     */
+    private function get_students_who_attempted_overview(): overviewitem {
+        global $CFG;
+        require_once($CFG->dirroot . '/mod/quiz/lib.php');
+        $numstudentattempted = quiz_num_users_who_attempted(
+            $this->quizsettings->get_quiz()->id,
+            $this->cm->id
+        );
+        $numstudentwhocanattempt = quiz_num_users_who_can_attempt(
+            $this->cm->id
+        );
+        $studentattemptedvalue = $this->stringmanager->get_string(
+            'count_of_total',
+            'core',
+            ['count' => $numstudentattempted, 'total' => $numstudentwhocanattempt]
+        );
+        // Add total entries.
+        return new overviewitem(
+            name: $this->stringmanager->get_string('studentswhoattempted', 'mod_quiz'),
+            value: html_to_text($studentattemptedvalue),
+            content: $studentattemptedvalue,
+            textalign: text_align::CENTER,
+        );
+    }
+
+    /**
+     * Get the "Total attempts" item.
+     *
+     * @return overviewitem The overview item.
+     */
+    private function get_total_attempts_overview(): overviewitem {
+        $numattempts = quiz_num_attempts($this->quizsettings->get_quiz(), $this->cm->get_course_module_record());
+
+        $overviewdialog = new overviewdialog(
+            buttoncontent:  $numattempts->total,
+            description: $this->stringmanager->get_string('totalattempts', 'mod_quiz'),
+            definition: ['buttonclasses' => button::SECONDARY_OUTLINE->classes() . ' dropdown-toggle'],
+        );
+        $allowedattempts = $this->quizsettings->get_quiz()->attempts;
+        if ($allowedattempts == 0) {
+            $allowedattempts = $this->stringmanager->get_string('attemptsunlimited', 'mod_quiz');
+        }
+        $overviewdialog->add_item(
+            $this->stringmanager->get_string('allowedattemptsperstudent', 'mod_quiz'),
+            $allowedattempts,
+        );
+        $numstudentattempted = quiz_num_users_who_attempted(
+            $this->quizsettings->get_quiz()->id,
+            $this->cm->id,
+        );
+        $numstudentwhocanattempt = quiz_num_users_who_can_attempt(
+            $this->cm->id
+        );
+        if ($numstudentwhocanattempt > 0 && $numstudentattempted > 0) {
+            $overviewdialog->add_item(
+                $this->stringmanager->get_string('averageattemptsperstudent', 'mod_quiz'),
+                round($numstudentattempted / $numstudentwhocanattempt)
+            );
+        }
+        // Add total entries.
+        return new overviewitem(
+            name: $this->stringmanager->get_string('totalattempts', 'mod_quiz'),
+            value: $numattempts->total,
+            content: $overviewdialog,
+            textalign: text_align::CENTER,
+        );
+    }
+}
